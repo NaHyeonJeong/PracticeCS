b6dce22dd797a04e12fb47940b504a65192efa5e
NaHyeonJeong <pigismile@gmail.com>
2022-03-19 18:08:08 +0900

    초기 커밋.

---
+++ PracticeLinq/SimpleLinq.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class SimpleLinq
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile() { Name = "나현정", Height = 154 },
+                new Profile() { Name = "나현진", Height = 160 },
+                new Profile() { Name = "김철수", Height = 170 },
+                new Profile() { Name = "최영희", Height = 140 },
+                new Profile() { Name = "알파고", Height = 169 },
+                new Profile() { Name = "테스트", Height = 159 }
+            };
+
+            var profiles = arrProfile
+                                .Where(profile => profile.Height < 170)
+                                .OrderBy(profile => profile.Height)
+                                .Select(profile => new
+                                {
+                                    Name = profile.Name,
+                                    InchHeight = profile.Height * 0.393
+                                });
+
+            foreach(var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.InchHeight}");
+        }
+    }
+}
---
+++ PractiveCS/FromLinq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiveCS
+{
+    class FromLinq
+    {
+        static void Main(string[] args)
+        {
+            int[] members = { 9,2,6,4,5,3,7,8,1,10};
+
+            var result = from n in members //n에는 실제로 데이터를 담지 X
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+
+            foreach(int n in result)
+                Console.WriteLine($"짝수 : {n}");
+        }
+    }
+}
